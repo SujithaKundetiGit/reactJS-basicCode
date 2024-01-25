0dbd6e36dac85ea05a7e3d69991d32ab651f8af6
vinod tonda <vinodtonda@gmail.com>
2024-01-25 10:38:27 +0530

    basic code commit react

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,27 @@
+/**const heading = React.createElement(
+    "h1",
+    {id:"heading"},
+    "Hello world using react!"
+    );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);*/ const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", "", "I am h2 tag")
+    ])
+], [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", "", "I am h2 tag")
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
